4ce7d258575dcf353c5b7cf1f6f48c0be5f7c5a6
Serhii Vasylenko <serhii.vasylenko@grammarly.com>
2024-01-13 03:00:50 +0100

    tflint

--- infra/dependencies.tf
+++
@@ -1,5 +0,0 @@
-data "aws_kms_alias" "ssm" {
-  name = "alias/aws/ssm"
-}
-data "aws_caller_identity" "this" {}
-data "aws_region" "this" {}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,12 +1,15 @@
 output "lambda_authorizer_environment_variables" {
+  description = "Environment variables for the lambda authorizer"
   value = module.lambda_api_gw_authorizer.lambda.environment[*].variables
 }
 
 output "lambda_calendar_backend_variables" {
+  description = "Environment variables for the lambda calendar backend"
   value = module.lambda_calendar_backend.lambda.environment[*].variables
 }
 
 output "apigateway_routes" {
+  description = "API Gateway endpoint and routes"
   value = {
     "Base URL" : aws_apigatewayv2_stage.default.invoke_url,
     "Calendars" : module.route_calendars.route.route_key,
